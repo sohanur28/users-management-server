a689fe6162ed6acbf2afce9afd9cb2c8cd2eeea2
sohanur28 <gsrahman28@gmail.com>
2023-05-14 17:07:47 +0600

    NewUser Add

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 const users = [
     {id: 1, name: 'Sabana', email: 'sabana@gmail.com'},
     {id: 2, name: 'Sabnoor', email: 'sabnoor@gmail.com'},
-    {id: 3, name: 'Sabila', email: 'sabila@gmail.com'}
+    {id: 3, name: 'Sabila', email: 'sabila@gmail.com'},
 ]
 
 app.get('/', (req, res) => {
@@ -21,7 +22,13 @@ app.get('/users', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
+    console.log('post api hitting')
     console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
+
 })
 
 app.listen(port, () => {
